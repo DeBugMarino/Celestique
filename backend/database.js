f9a5f638ff55643bf3b4e7a3a8d59b5874d7cfdd
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-06-23 18:07:01 +0200

    inizio settaggio backend con render

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ import pgPromise from "pg-promise";
 import dotenv from "dotenv";
 dotenv.config();
 const dataBase = pgPromise();
-const db = pgPromise({
+const db = pgPromise()({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME,
