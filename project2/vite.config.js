72c6acbf613413c688d85c9d22c79eb883b0cbf7
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-06-25 12:24:15 +0200

    change server for error cross origin

--- project2/vite.config.js
+++ project2/vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FS34-Team1-Project2/",
+  base: "/Celestique/",
   plugins: [react(), tailwindcss()],
 
   build: {
     outDir: "dist",
+    sourcemap: true, // Abilita le sourcemap
+    minify: false,
   },
 });
